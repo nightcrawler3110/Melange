20442a14754e713d39bbfd84faf608612139ddac
nightcrawler3110 <jaloreeshaily@gmail.com>
2020-09-25 23:42:35 +0530

    Final Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://ec2-3-80-125-55.compute-1.amazonaws.com:3000'
+  // serverUrl: 'http://ec2-3-80-125-55.compute-1.amazonaws.com:3000'
+
+  serverUrl: 'http://localhost:3000'
 
 };
